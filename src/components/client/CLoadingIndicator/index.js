5e0a9a1d2edc697eba8edc366ae509c2fabaa1f1
Thu Nguyen <hoaithu7604@gmail.com>
2020-05-14 21:26:37 +0900

    delete

--- src/components/client/CLoadingIndicator/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./LoadingIndicator.scss";
-
-const CLoadingIndicator = () => <div className="loader" />;
-
-export default CLoadingIndicator;
--- src/components/client/CNavBar/CHoverContainer/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./HoverContainer.scss";
-
-const HoverContainer = ({ children, className }) => (
-  <div className={`hover-container ${className}`}>{children}</div>
-);
-
-export default HoverContainer;
