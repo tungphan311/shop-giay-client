c1c839059b60f7e3f6ef1975825680bab46511c8
tungphan311 <thanhtunga1lqd@gmail.com>
2020-05-14 21:02:06 +0700

    ad api url to .env and ignore .env

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 const API = axios.create({
   baseURL: BASE_URL,
 });
