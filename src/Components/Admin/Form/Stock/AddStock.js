ad3baa452ec4e0658bb647c0cf4a0de73cc56d22
tungphan311 <thanhtunga1lqd@gmail.com>
2020-05-21 23:41:18 +0700

    modify ui

--- src/Components/Admin/Form/Stock/AddStock.js
+++ src/Components/Admin/Form/Stock/AddStock.js
@@ -4,15 +4,17 @@ import { FORM_KEY_ADDSHOES } from "state/reducers/formReducer";
 
 function AAddStock({ handleSubmit, previousPage }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>assd</div>
-      <div style={{ marginLeft: "auto", marginRight: 0 }} className="mt-5">
-        <button className="btn btn-primary btn-border" onClick={previousPage}>
-          Trở về
-        </button>
-        <button type="submit" className="btn btn-primary">
-          Hoàn tất
-        </button>
+    <form className="AddShoesForm" onSubmit={handleSubmit}>
+      <div className="container">
+        <div>assd</div>
+        <div style={{ marginLeft: "auto", marginRight: 0 }} className="mt-5">
+          <button className="btn btn-primary btn-border" onClick={previousPage}>
+            Trở về
+          </button>
+          <button type="submit" className="btn btn-primary ml-2">
+            Hoàn tất
+          </button>
+        </div>
       </div>
     </form>
   );
