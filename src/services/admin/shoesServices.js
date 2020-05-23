606c15cf1ef932dec7a398f2303b7be8ef01eea7
tungphan311 <thanhtunga1lqd@gmail.com>
2020-05-23 17:27:24 +0700

    creatale select for provider, size and color

--- src/services/admin/shoesServices.js
+++ src/services/admin/shoesServices.js
@@ -11,3 +11,19 @@ export async function getProviders() {
 export async function addProviders({ name }) {
   return await API.post("/provider", { name });
 }
+
+export async function getColors() {
+  return await API.get("/color");
+}
+
+export async function addColor({ name }) {
+  return await API.post("/color", { name });
+}
+
+export async function getSizes() {
+  return await API.get("/size");
+}
+
+export async function addSize({ name }) {
+  return await API.post("/size", { name });
+}
