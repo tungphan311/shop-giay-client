fbd177873672f88626cfa18a0f2307ec252eed9f
tungphan311 <thanhtunga1lqd@gmail.com>
2020-05-28 23:56:11 +0700

    prevent default

--- src/Components/Admin/Sidebar/SubItem.js
+++ src/Components/Admin/Sidebar/SubItem.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import history from "state/history";
 
 function ASubItem({ href, title }) {
   return (
     <li>
-      <a href={href}>
+      <a
+        href={href}
+        onClick={(e) => {
+          e.preventDefault();
+          history.push(href);
+        }}
+      >
         <span className="sub-item">{title}</span>
       </a>
     </li>
