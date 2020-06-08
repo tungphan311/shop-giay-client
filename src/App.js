2de1cdb828d2f4bdc01f83186c15e82229f09444
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-08 18:01:20 +0700

    loading screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { connect } from "react-redux";
 import { ACTION_GET_INIT_DATA } from "./state/reducers/cInitReducer";
 import Toastify from "Components/client/CToaster/Toasify";
 import MessengerCustomerChat from "react-messenger-customer-chat";
+import LoadingScreen from "Components/Admin/Loading/LoadingScreen";
+
+const mapStateToProps = (state) => ({
+  loading: state.aLoading.isLoading,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   cInitData: () => dispatch({ type: ACTION_GET_INIT_DATA }),
@@ -18,6 +23,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Toastify />
+        <LoadingScreen show={loading} />
         {children}
         <MessengerCustomerChat
           pageId="100404311703422"
@@ -28,4 +34,4 @@ class App extends React.Component {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
