86a100edd988985d400aa1ba61921f88e655c742
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-13 02:21:53 +0700

    temp filter bar

---
+++ src/Components/Admin/Svg/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const FilterIcon = () => (
+  <svg className="svg-next-icon svg-next-icon-size-14" width="14" height="14">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459">
+      <g>
+        <path d="M178.5,382.5h102v-51h-102V382.5z M0,76.5v51h459v-51H0z M76.5,255h306v-51h-306V255z"></path>
+      </g>
+    </svg>
+  </svg>
+);
+
+export const SearchIcon = () => (
+  <svg className="svg-next-icon svg-next-icon-size-18" width="18" height="18">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M8 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8m9.707 4.293l-4.82-4.82A5.968 5.968 0 0 0 14 8 6 6 0 0 0 2 8a6 6 0 0 0 6 6 5.968 5.968 0 0 0 3.473-1.113l4.82 4.82a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414"></path>
+    </svg>
+  </svg>
+);
+
+export const AddIcon = () => (
+  <svg className="svg-next-icon svg-next-icon-size-16" width="16" height="16">
+    <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.99992 0.333374C3.31992 0.333374 0.333252 3.32004 0.333252 7.00004C0.333252 10.68 3.31992 13.6667 6.99992 13.6667C10.6799 13.6667 13.6666 10.68 13.6666 7.00004C13.6666 3.32004 10.6799 0.333374 6.99992 0.333374ZM6.33325 3.66671V6.33337H3.66658V7.66671H6.33325V10.3334H7.66658V7.66671H10.3333V6.33337H7.66658V3.66671H6.33325ZM1.66659 7.00004C1.66659 9.94004 4.05992 12.3334 6.99992 12.3334C9.93992 12.3334 12.3333 9.94004 12.3333 7.00004C12.3333 4.06004 9.93992 1.66671 6.99992 1.66671C4.05992 1.66671 1.66659 4.06004 1.66659 7.00004Z"
+      ></path>
+    </svg>
+  </svg>
+);
