6caa086e2886be81a91eab31f7839e69a4e95839
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-12 18:37:33 +0700

    pagination

--- src/Components/Admin/Pagination/Pagination.js
+++ src/Components/Admin/Pagination/Pagination.js
@@ -2,9 +2,33 @@ import React from "react";
 import Pagination from "react-js-pagination";
 import "./Pagination.scss";
 
-function APagination({ page, handlePageChange, perPage, totalRows }) {
+function APagination({
+  page,
+  handlePageChange,
+  perPage,
+  totalRows,
+  pageSizes,
+  handlePerPageChange,
+}) {
+  const start = (page - 1) * perPage + 1;
+  const end = page * perPage;
   return (
-    <div className="pagination-admin">
+    <div className="pagination-admin mr-4">
+      <span>Số dòng/ trang: </span>
+      <select
+        className="ml-2 mr-2 pagination__page-size"
+        value={perPage}
+        onChange={handlePerPageChange}
+      >
+        {pageSizes.map((p) => (
+          <option key={p} value={p}>
+            {p}
+          </option>
+        ))}
+      </select>
+      <span className="mr-2">{`${start} - ${
+        end > totalRows ? totalRows : end
+      } trên ${totalRows}`}</span>
       <Pagination
         activePage={page}
         itemsCountPerPage={perPage}
