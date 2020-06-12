a88938408c81d690c9224bd25ffaa281a5d704cc
tungphan311 <thanhtunga1lqd@gmail.com>
2020-06-13 03:28:09 +0700

    change no data component

--- src/Components/Admin/Svg/index.js
+++ src/Components/Admin/Svg/index.js
@@ -29,3 +29,121 @@ export const AddIcon = () => (
     </svg>
   </svg>
 );
+
+export const CloseIcon = () => (
+  <span className="d-inline-block">
+    <svg
+      className="svg-next-icon cursor-pointer svg-next-icon-size-16"
+      width="16"
+      height="16"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" version="1.1">
+        <path d="M17,15.584 L13.4,11.984 L17,8.384 L15.584,6.968 L11.984,10.568 L8.384,6.968 L6.968,8.384 L10.568,11.984 L6.968,15.584 L8.384,17 L11.984,13.4 L15.584,17 L17,15.584 Z M11.984,2 C13.8080091,2 15.4959922,2.45599544 17.048,3.368 C18.5520075,4.23200432 19.7359957,5.41599248 20.6,6.92 C21.5120046,8.47200776 21.968,10.1599909 21.968,11.984 C21.968,13.8080091 21.5120046,15.4959922 20.6,17.048 C19.7359957,18.5520075 18.5520075,19.7359957 17.048,20.6 C15.4959922,21.5120046 13.8080091,21.968 11.984,21.968 C10.1599909,21.968 8.47200776,21.5120046 6.92,20.6 C5.41599248,19.7199956 4.23200432,18.5280075 3.368,17.024 C2.45599544,15.4719922 2,13.792009 2,11.984 C2,10.175991 2.45599544,8.49600776 3.368,6.944 C4.2480044,5.43999248 5.43999248,4.2480044 6.944,3.368 C8.49600776,2.45599544 10.175991,2 11.984,2 Z"></path>
+      </svg>
+    </svg>
+  </span>
+);
+
+export const Box = () => (
+  <svg
+    className="svg-next-icon mb-5"
+    width="100"
+    height="100"
+    style={{ color: "#c3cfd8", width: "100px", height: "100px" }}
+  >
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 208 230"
+    >
+      <defs>
+        <linearGradient
+          id="linear-gradient"
+          x1="133.69"
+          y1="120.61"
+          x2="73.06"
+          y2="91.22"
+          gradientTransform="matrix(1, 0, 0, -1, 0, 231.28)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#95abc2"></stop>
+          <stop offset="1" stopColor="#fff"></stop>
+        </linearGradient>
+        <mask
+          id="mask"
+          x="134.5"
+          y="14.55"
+          width="17.97"
+          height="17.21"
+          maskUnits="userSpaceOnUse"
+        >
+          <g id="mask0">
+            <path
+              d="M137,24a8.1,8.1,0,0,1,8.1-8.1h6.3v6.3a8.1,8.1,0,0,1-8.1,8.1H137Z"
+              fill="#fff"
+            ></path>
+          </g>
+        </mask>
+      </defs>
+      <title>Artwork</title>
+      <path d="M94.2,85.7l85.9,23.6v82.6l-85.9-37Z" fill="#8e99a3"></path>
+      <path d="M94.3,85.7,24.1,105.3v86.8l70.1-37.6Z" fill="#a0aab5"></path>
+      <path
+        d="M139.4,185.9c-19.4-5.7-53.4-5.2-66.2-38.3-11.7-30.4,31.5-59.6,43.6-46.8,8.6,9.1-2.4,22.2-17.7,20.7C82,119.8,71.9,104.5,75.3,88c6.7-32.7,58.3-54.4,60.5-55.9"
+        fill="none"
+        strokeWidth="3"
+        strokeDasharray="6 7"
+        stroke="url(#linear-gradient)"
+      ></path>
+      <path
+        d="M141,21.5s-.5-12.3-3.7-15.6a8.4,8.4,0,0,0-11.8,11.8C128.7,21,141,21.5,141,21.5Z"
+        fill="#ced8e5"
+        opacity="0.4"
+      ></path>
+      <path
+        d="M146.7,26.4s.5,12.3,3.7,15.6a8.4,8.4,0,0,0,11.8-11.8C159.1,26.9,146.7,26.4,146.7,26.4Z"
+        fill="#ced8e5"
+        opacity="0.4"
+      ></path>
+      <path
+        d="M137,24a8.1,8.1,0,0,1,8.1-8.1h6.3v6.3a8.1,8.1,0,0,1-8.1,8.1H137Z"
+        fill="#a8b7c5"
+      ></path>
+      <g mask="url(#mask)">
+        <rect
+          x="140.1"
+          y="16.7"
+          width="2.3"
+          height="16.69"
+          transform="translate(23.6 107.2) rotate(-45)"
+          fill="#dde5f2"
+        ></rect>
+        <path d="M139.1,16.2l1.6-1.6,11.8,11.8L150.9,28Z" fill="#dde5f2"></path>
+      </g>
+      <path
+        d="M159.2,12.3l5.5-1.3"
+        fill="none"
+        stroke="#8696c5"
+        strokeWidth="0.9"
+      ></path>
+      <path
+        d="M156.9,10.1l2.1-5.5"
+        fill="none"
+        stroke="#8696c5"
+        strokeWidth="0.9"
+      ></path>
+      <path
+        d="M153.7,20.5a6.1,6.1,0,1,0-6.1-6.1A6.1,6.1,0,0,0,153.7,20.5Z"
+        fill="#8b9aa8"
+      ></path>
+      <path d="M24.1,105.5,110,132.8V229L24.1,192Z" fill="#dae2ec"></path>
+      <path d="M180.5,109.2l-71.7,23.6V229l71.7-37Z" fill="#c5d0d9"></path>
+      <path
+        d="M180.3,109.1,208,146.2l-73.6,29L109,132.6Z"
+        fill="#dae2ec"
+      ></path>
+      <path d="M24,105.3,0,142.8l84.6,32.4,24.7-42.7Z" fill="#c5d0d9"></path>
+    </svg>
+  </svg>
+);
