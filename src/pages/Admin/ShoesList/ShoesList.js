133b60fdba17d23e67e19daa463a7b5539cce1f7
Phan Thanh Tùng <tung@Phans-MacBook-Pro.local>
2020-06-15 16:04:10 +0700

    export to csv

--- src/pages/Admin/ShoesList/ShoesList.js
+++ src/pages/Admin/ShoesList/ShoesList.js
@@ -11,6 +11,8 @@ import history from "state/history";
 import swal from "sweetalert";
 import qs from "query-string";
 import APagination from "Components/Admin/Pagination/Pagination";
+import AFilterBar from "Components/Admin/FilterBar/FilterBar";
+import { formatDateToString } from "utils/helper";
 
 function AShoesList({ location: { search } }) {
   // state
@@ -162,10 +164,60 @@ function AShoesList({ location: { search } }) {
     fetchShoes(page, pageSize);
   };
 
+  const convertArrayOfObjectsToCSV = (array) => {
+    console.log(array);
+    let result;
+
+    const columnDelimiter = ",";
+    const lineDelimiter = "\n";
+    const keys = Object.keys(data[0]);
+
+    result = "";
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+    array.forEach((item) => {
+      let ctr = 0;
+      keys.forEach((key) => {
+        if (ctr > 0) result += columnDelimiter;
+
+        result += item[key];
+
+        ctr++;
+      });
+      result += lineDelimiter;
+    });
+
+    return result;
+  };
+
+  const downloadCSV = (source) => {
+    let arr = [];
+    if (source === "current") {
+      arr = data;
+    }
+
+    const link = document.createElement("a");
+
+    let csv = convertArrayOfObjectsToCSV(arr);
+    if (!csv) return;
+
+    const date = formatDateToString(new Date());
+    const fileName = `shoes-${date}.csv`;
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = `data:text/csv;charset=utf-8,${csv}`;
+    }
+
+    link.setAttribute("href", encodeURI(csv));
+    link.setAttribute("download", fileName);
+    link.click();
+  };
+
   return (
     <div>
       <ABreadcrumb title="Tất cả sản phẩm" list={BREADCRUMB} />
-      <div className="row">
+      <AFilterBar onExport={(source) => downloadCSV(source)} />
+      <div className="row mt-5">
         <div className="col-md-12">
           <div className="card">
             <DataTable
