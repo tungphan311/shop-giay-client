593cc25c7d39b8b3ee1e89a6366614ca93d69890
Dang Quang <dangquang9a@gmail.com>
2020-07-13 07:54:22 +0700

    init

--- src/services/cCustomerService.js
+++ src/services/cCustomerService.js
@@ -9,10 +9,19 @@ export async function cGetAddresses() {
 }
 
 export async function cAddorUpdateAddress(data) {
-  const path = data.id ? "client/customer/update-address" : "client/customer/add-address";
+  const path = data.id
+    ? "client/customer/update-address"
+    : "client/customer/add-address";
   const token = localStorage.getItem(TOKEN_KEY);
   const AuthStr = "Bearer " + token;
   return await API.post(path, data, {
     headers: { Authorization: AuthStr },
   });
+}
+
+export async function cGetCustomerInfo() {
+  const path = "client/customer/getInfo";
+  const token = localStorage.getItem(TOKEN_KEY);
+  const AuthStr = "Bearer " + token;
+  return await API.get(path, { headers: { Authorization: AuthStr } });
 }
